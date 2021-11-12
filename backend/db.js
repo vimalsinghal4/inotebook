7a281f3f264255f95dde2febb9d2ae9bb516ba4f
Vimal Singhal <vimalsinghal33@gmail.com>
2021-11-12 19:19:51 +0530

    hello 2

---
+++ backend/db.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const mongoURI = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
+const connectToMongo=()=>{
+    mongoose.connect(mongoURI,()=>{
+        console.log("connected successfully");
+    })
+}
+module.exports=connectToMongo;
